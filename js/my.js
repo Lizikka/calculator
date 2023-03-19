be90991025b02438bbe4128f569d456c38187ebc
Liza Volyanskaya <you@example.com>
2023-03-19 13:07:00 +0200

    2

--- js/my.js
+++ js/my.js
@@ -1,11 +1,15 @@
 
     let btn = document.querySelector(`#card`)
+    let svn = document.querySelector(`#seventh`)
     let calcScreen = document.querySelector(`#pole`)
     
     btn.addEventListener(`click`, ()=>{
         calcScreen.innerHTML = `Rostik!`
     }
     )
+    svn.addEventListener(`click`, ()=>{
+        calcScreen.innerHTML = `Elisa!`
+    })
 
 
 
